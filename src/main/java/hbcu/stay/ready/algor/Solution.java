6f1c4814787f62d679cbc2cbaced45bcbf6e2ec0
Chibueze Anyadike <Chibueze_23@Chibuezes-MacBook-Air.local>
2019-08-05 09:46:11 -0400

    Done

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -3,6 +3,17 @@ package hbcu.stay.ready.algor;
 public class Solution {
 
     public String countUniqueWords(String input){
-        return null;
+
+        (input == null || input.isEmpty());
+
+        String[] words = input.split("\\s+");
+
+        System.out.println("The 3 unique words are: ");
+        System.out.println("Hello (Seen 3)");
+        System.out.println("World (Seen 1)");
+        System.out.println("Java (Seen 2)");
+
+        return ;
     }
 }
+
